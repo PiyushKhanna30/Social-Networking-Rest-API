4c31bf5ccdcf92e5932f0d63277cc7a53da67eba
piyushkhanna <khanna.piyush30@gmail.com>
2022-09-06 16:21:05 +0530

    Basic setup + Global Exception Handling + Swagger

---
+++ src/main/java/com/piyush/rest/webservices/user/User.java
@@ -0,0 +1,53 @@
+package com.piyush.rest.webservices.user;
+
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class User {
+    private Integer id;
+    @Size(min=2, message = "Name should have atleast 2 characters")
+    private String name;
+    @Past(message = "Birth Date should be in the past")
+    private LocalDate dateOfBirth;
+
+    public User(Integer id, String name, LocalDate dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
