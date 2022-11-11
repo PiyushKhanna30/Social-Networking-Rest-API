4a9208e2b6a0848b8353446854027039c9e406c9
piyushkhanna <khanna.piyush30@gmail.com>
2022-11-11 11:27:08 +0530

    fix

--- src/main/java/com/piyush/rest/webservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/piyush/rest/webservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -5,12 +5,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
+@ControllerAdvice
+@RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
